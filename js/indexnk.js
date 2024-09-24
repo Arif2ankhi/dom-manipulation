90d7c74f021fb5e3990522d1adb6df334a013e31
Arif2ankhi <arif2ankhi@gmail.com>
2024-09-24 13:25:34 +0100

    started the js files and added event handlers

--- js/indexnk.js
+++ js/indexnk.js
@@ -1,1 +1,42 @@
-console.log('testing');
+// Toggle buttons
+
+const historyButton = document.getElementById('btn-history');
+const donationButton = document.getElementById('btn-donation');
+
+// Button for different sections
+
+const noakhaliButton = document.getElementById('button-noakhali');
+const feniButton = document.getElementById('button-feni');
+const quotaButton = document.getElementById('button-quota');
+
+//  Toogle sections
+
+const donationSection = document.getElementById('donation-section');
+const historySection = document.getElementById('history-section');
+
+
+// Click handlers 
+
+historyButton.addEventListener('click', function(){
+    donationSection.classList.add('hidden');
+    historySection.classList.add('hidden');
+
+    donationButton.classList.add('bg-[##4ADE80]', 'text-black');
+    donationButton.classList.remove('border-3', 'border-gray', 'text-gray');
+
+    historyButton.classList.remove('bg-[##4ADE80]', 'text-black');
+    historyButton.classList.add('border-3', 'border-gray', 'text-gray')
+});
+
+
+donationButton.addEventListener('click', function(){
+    donationSection.classList.remove('hidden');
+    historySection.classList.add('hidden');
+
+    donationButton.classList.add()
+})
+
+// Donation for all 3 sections:
+
+noakhaliButton
+
