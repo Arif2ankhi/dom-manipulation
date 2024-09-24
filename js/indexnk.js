6c4f9678d00c0bf8cbe9407de2c871f9ed24d27f
Arif2ankhi <arif2ankhi@gmail.com>
2024-09-24 15:05:46 +0100

    Added calculate  in the js file

--- js/indexnk.js
+++ js/indexnk.js
@@ -17,26 +17,74 @@ const historySection = document.getElementById('history-section');
 
 // Click handlers 
 
+// History  add remove
+
 historyButton.addEventListener('click', function(){
     donationSection.classList.add('hidden');
     historySection.classList.add('hidden');
 
-    donationButton.classList.add('bg-[##4ADE80]', 'text-black');
+    donationButton.classList.add('bg-[#4ADE80]', 'text-black');
     donationButton.classList.remove('border-3', 'border-gray', 'text-gray');
 
-    historyButton.classList.remove('bg-[##4ADE80]', 'text-black');
+    historyButton.classList.remove('bg-[#4ADE80]', 'text-black');
     historyButton.classList.add('border-3', 'border-gray', 'text-gray')
 });
 
+// donation add remove
 
 donationButton.addEventListener('click', function(){
     donationSection.classList.remove('hidden');
     historySection.classList.add('hidden');
 
-    donationButton.classList.add()
+    donationButton.classList.add('bg-[#4ADE80]', 'text-black');
+    donationButton.classList.remove('border-3', 'border-gray', 'text-gray');
+
+    historyButton.classList.remove('bg-[#4ADE80]', 'text-black');
+    historyButton.classList.add('border-3', 'border-gray', 'text-gray')
 })
 
-// Donation for all 3 sections:
+// Donation for all 3 locations/areas:
 
-noakhaliButton
+noakhaliButton.addEventListener('click', function(){
+
+    const availableBalance = getBalanceById('available-balance');
+    const inputMoney = getInputValueByID('input-value-noakhali');
+    const noakhaliBalance = getBalanceById('available-balance-noakhali');
+    const noakhaliHeading = document.getElementById('noakhali-heading').innerText;
+
+    calculate(availableBalance, inputMoney, noakhaliBalance, 'available-balance-noakhali',noakhaliHeading);
+
+});
+
+feniButton.addEventListener('click', function(){
+
+    const availableBalance = getBalanceById('available-balance');
+    const inputMoney = getInputValueByID('input-value-feni');
+    const feniBalance = getBalanceById('available-balance-feni');
+    const feniHeading = document.getElementById('feni-heading').innerText;
+
+    calculate(availableBalance, inputMoney, feniBalance, 'available-balance-feni',feniHeading);
+
+});
+feniButton.addEventListener('click', function(){
+
+    const availableBalance = getBalanceById('available-balance');
+    const inputMoney = getInputValueByID('input-value-feni');
+    const feniBalance = getBalanceById('available-balance-feni');
+    const feniHeading = document.getElementById('feni-heading').innerText;
+
+    calculate(availableBalance, inputMoney, feniBalance, 'available-balance-feni',feniHeading);
+
+});
+
+quotaButton.addEventListener('click', function(){
+
+    const availableBalance = getBalanceById('available-balance');
+    const inputMoney = getInputValueByID('input-value-quota');
+    const quotaBalance = getBalanceById('available-balance-quota');
+    const quotaHeading = document.getElementById('quota-heading').innerText;
+
+    calculate(availableBalance, inputMoney, quotaBalance, 'available-balance-quota',quotaHeading);
+
+});
 
