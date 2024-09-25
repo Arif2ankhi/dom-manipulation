55a141f079f7880b708347ddc70807e372840860
Arif2ankhi <arif2ankhi@gmail.com>
2024-09-25 01:54:49 +0100

    Modifiey calculation and edited footer

--- js/index.js
+++ js/index.js
@@ -1,43 +1,13 @@
-// menu button
-// const historyBtn = document.getElementById('history-btn');
-// const donationBtn = document.getElementById('donation-btn');
-// sections
+
 const donationSec =  document.getElementById('donation-sec');
 const historySec = document.getElementById('history-sec');
-// donation buttons
+
+
 const noakhaliBtn = document.getElementById('noakhali-btn')
 const feniBtn = document.getElementById('feni-btn')
-const quotaBtn = document.getElementById('quota-btn')
-
-// menu btn click handles
-// historyBtn.addEventListener('click',function(){
-//     donationSec.classList.add('hidden');
-//     historySec.classList.remove('hidden');
-
-//     historyBtn.classList.add('bg-[#B4F461]','text-black')
-//     historyBtn.classList.remove('border-2' ,'border-[#111111B3]','text-[#111111B3]')
-
-//     donationBtn.classList.remove('bg-[#B4F461]','text-black')
-//     donationBtn.classList.add('border-2' ,'border-[#111111B3]','text-[#111111B3]')
+const quotaBtn = document.getElementById('quota-btn');
 
 
-// })
-
-// donationBtn.addEventListener('click',function(){
-//     donationSec.classList.remove('hidden');
-//     historySec.classList.add('hidden');
-
-//     donationBtn.classList.add('bg-[#B4F461]','text-black')
-//     donationBtn.classList.remove('border-2' ,'border-[#111111B3]','text-[#111111B3]')
-
-//     historyBtn.classList.remove('bg-[#B4F461]','text-black')
-//     historyBtn.classList.add('border-2' ,'border-[#111111B3]','text-[#111111B3]')
-
-// })
-
-// donation handles all
-
-// noakhali
 noakhaliBtn.addEventListener('click',function(){
     
     const myBalance = getBalance('my-balance')
@@ -47,10 +17,8 @@ noakhaliBtn.addEventListener('click',function(){
 
     calculate(inputAmount,myBalance,noakhaliBalance,'noakhali-balance',noakhaliTitle);
 
-})
+});
 
-
-// feni
 feniBtn.addEventListener('click',function(){
     
     const myBalance = getBalance('my-balance')
@@ -60,10 +28,8 @@ feniBtn.addEventListener('click',function(){
 
     calculate(inputAmount,myBalance,feniBalance,'feni-balance',feniTitle);
 
-})
+});
 
-
-// quota
 quotaBtn.addEventListener('click',function(){
     
     const myBalance = getBalance('my-balance')
@@ -73,4 +39,4 @@ quotaBtn.addEventListener('click',function(){
 
     calculate(inputAmount,myBalance,quotaBalance,'quota-balance',quotaTitle);
 
-})
+});
