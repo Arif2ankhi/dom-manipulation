7edb1e9e51f055c5d6b63c6d1d302ae4181a0728
Arif2ankhi <arif2ankhi@gmail.com>
2024-09-25 01:03:04 +0100

    Added modal

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // menu button
-const historyBtn = document.getElementById('history-btn');
-const donationBtn = document.getElementById('donation-btn');
+// const historyBtn = document.getElementById('history-btn');
+// const donationBtn = document.getElementById('donation-btn');
 // sections
 const donationSec =  document.getElementById('donation-sec');
 const historySec = document.getElementById('history-sec');
@@ -10,30 +10,30 @@ const feniBtn = document.getElementById('feni-btn')
 const quotaBtn = document.getElementById('quota-btn')
 
 // menu btn click handles
-historyBtn.addEventListener('click',function(){
-    donationSec.classList.add('hidden');
-    historySec.classList.remove('hidden');
+// historyBtn.addEventListener('click',function(){
+//     donationSec.classList.add('hidden');
+//     historySec.classList.remove('hidden');
 
-    historyBtn.classList.add('bg-[#B4F461]','text-black')
-    historyBtn.classList.remove('border-2' ,'border-[#111111B3]','text-[#111111B3]')
+//     historyBtn.classList.add('bg-[#B4F461]','text-black')
+//     historyBtn.classList.remove('border-2' ,'border-[#111111B3]','text-[#111111B3]')
 
-    donationBtn.classList.remove('bg-[#B4F461]','text-black')
-    donationBtn.classList.add('border-2' ,'border-[#111111B3]','text-[#111111B3]')
+//     donationBtn.classList.remove('bg-[#B4F461]','text-black')
+//     donationBtn.classList.add('border-2' ,'border-[#111111B3]','text-[#111111B3]')
 
 
-})
+// })
 
-donationBtn.addEventListener('click',function(){
-    donationSec.classList.remove('hidden');
-    historySec.classList.add('hidden');
+// donationBtn.addEventListener('click',function(){
+//     donationSec.classList.remove('hidden');
+//     historySec.classList.add('hidden');
 
-    donationBtn.classList.add('bg-[#B4F461]','text-black')
-    donationBtn.classList.remove('border-2' ,'border-[#111111B3]','text-[#111111B3]')
+//     donationBtn.classList.add('bg-[#B4F461]','text-black')
+//     donationBtn.classList.remove('border-2' ,'border-[#111111B3]','text-[#111111B3]')
 
-    historyBtn.classList.remove('bg-[#B4F461]','text-black')
-    historyBtn.classList.add('border-2' ,'border-[#111111B3]','text-[#111111B3]')
+//     historyBtn.classList.remove('bg-[#B4F461]','text-black')
+//     historyBtn.classList.add('border-2' ,'border-[#111111B3]','text-[#111111B3]')
 
-})
+// })
 
 // donation handles all
 
@@ -74,94 +74,3 @@ quotaBtn.addEventListener('click',function(){
     calculate(inputAmount,myBalance,quotaBalance,'quota-balance',quotaTitle);
 
 })
-
-
-
-// const historyButton = document.getElementById('btn-history');
-// const donationButton = document.getElementById('btn-donation');
-
-// // Button for different sections
-
-// const noakhaliButton = document.getElementById('button-noakhali');
-// const feniButton = document.getElementById('button-feni');
-// const quotaButton = document.getElementById('button-quota');
-
-// //  Toogle sections
-
-// const donationSection = document.getElementById('donation-section');
-// const historySection = document.getElementById('history-section');
-
-
-// // Click handlers 
-
-// // History  add remove
-
-// historyButton.addEventListener('click', function(){
-//     donationSection.classList.add('hidden');
-//     historySection.classList.add('hidden');
-
-//     donationButton.classList.add('bg-[#4ADE80]', 'text-black');
-//     donationButton.classList.remove('border-3', 'border-gray', 'text-gray');
-
-//     historyButton.classList.remove('bg-[#4ADE80]', 'text-black');
-//     historyButton.classList.add('border-3', 'border-gray', 'text-gray')
-// });
-
-// // donation add remove
-
-// donationButton.addEventListener('click', function(){
-//     donationSection.classList.remove('hidden');
-//     historySection.classList.add('hidden');
-
-//     donationButton.classList.add('bg-[#4ADE80]', 'text-black');
-//     donationButton.classList.remove('border-3', 'border-gray', 'text-gray');
-
-//     historyButton.classList.remove('bg-[#4ADE80]', 'text-black');
-//     historyButton.classList.add('border-3', 'border-gray', 'text-gray')
-// })
-
-// // Donation for all 3 locations/areas:
-
-// noakhaliButton.addEventListener('click', function(){
-
-//     const availableBalance = getBalanceById('available-balance');
-//     const inputMoney = getInputValueByID('input-value-noakhali');
-//     const noakhaliBalance = getBalanceById('available-balance-noakhali');
-//     const noakhaliHeading = document.getElementById('noakhali-heading').innerText;
-
-//     calculate(availableBalance, inputMoney, noakhaliBalance, 'available-balance-noakhali',noakhaliHeading);
-
-// });
-
-// feniButton.addEventListener('click', function(){
-
-//     const availableBalance = getBalanceById('available-balance');
-//     const inputMoney = getInputValueByID('input-value-feni');
-//     const feniBalance = getBalanceById('available-balance-feni');
-//     const feniHeading = document.getElementById('feni-heading').innerText;
-
-//     calculate(availableBalance, inputMoney, feniBalance, 'available-balance-feni',feniHeading);
-
-// });
-// feniButton.addEventListener('click', function(){
-
-//     const availableBalance = getBalanceById('available-balance');
-//     const inputMoney = getInputValueByID('input-value-feni');
-//     const feniBalance = getBalanceById('available-balance-feni');
-//     const feniHeading = document.getElementById('feni-heading').innerText;
-
-//     calculate(availableBalance, inputMoney, feniBalance, 'available-balance-feni',feniHeading);
-
-// });
-
-// quotaButton.addEventListener('click', function(){
-
-//     const availableBalance = getBalanceById('available-balance');
-//     const inputMoney = getInputValueByID('input-value-quota');
-//     const quotaBalance = getBalanceById('available-balance-quota');
-//     const quotaHeading = document.getElementById('quota-heading').innerText;
-
-//     calculate(availableBalance, inputMoney, quotaBalance, 'available-balance-quota',quotaHeading);
-
-// });
-
--- js/utilities.js
+++ js/utilities.js
@@ -1,4 +1,3 @@
-
 function getInputValue(id){
     return parseFloat(document.getElementById(id).value);
 }
@@ -20,61 +19,4 @@ function calculate(inputAmount,myBalance,locationBalance,id,locationTitle){
         document.getElementById('my-balance').innerText = (myBalance - inputAmount).toFixed(2);
         history(inputAmount,locationTitle);
     }
-}
-
-function history(inputAmount,locationTitle){
-    const div = document.createElement('div');
-    const now = new Date()
-    const dateTimeString = now.toString();
-    div.classList.add( 'flex', 'flex-col', 'gap-4', 'md:p-8', 'p-4', 'border-2', 'border-[#111111B3]', 'rounded-xl');
-    div.innerHTML = `
-        <h3 class="md:text-xl font-extrabold text-black">
-            ${inputAmount} Taka donated to: ${locationTitle}
-        </h3>
-        <p class="text-[#111111B3] bg-[#f9f7f3] rounded-lg px-4 py-2">Date : 
-            ${dateTimeString}
-        </p>   
-    `;
-    document.getElementById('history-div').appendChild(div);
-}
-
-
-// function getInputValueByID(id){
-//     return parseFloat(document.getElementById(id).value);
-// };
-
-// function getBalanceById(id){
-//     return parseFloat(document.getElementById(id).innerText);
-// };
-
-// function calculate(inputMoney,availableBalance,locationBalance,id,locationHeading){
-//     if(inputMoney < 0 || isNaN(inputMoney)){
-//         alert('Invalid Donation Amount')
-//     }
-//     else if(availableBalance < inputMoney){
-//         alert('Do not have Enough Balance')
-//     }else{
-//         my-modal.showModal();
-//         document.getElementById(id).innerText = (locationBalance + inputMoney).toFixed(2);
-//         document.getElementById('available-balance').innerText = (availableBalance -inputMoney).toFixed(2);
-//         history(inputMoney, locationHeading);
-
-//     }
-// };
-
-// function history(inputMoney,locationHeading){
-//     const div = document.createElement('div');
-//     const now = new Date()
-//     const dateTimeString = now.toString();
-//     div.classList.add('flex', 'flex-col', 'gap-4', 'md:p-8', 'p-4', 'border-2', 'border-gray', 'rounded-xl'),
-//     div.innerHTML =`
-//     <h3 class="md:text-xl font-extrabold text-black">
-//     ${inputMoney} Taka  donate to: ${locationHeading}
-//     </h3>
-//     <p class="text-gray bg-slate-400 rounded-lg px-4 py-2">Date:
-//     ${dateTimeString}
-//     </p>
-    
-//     `;
-//     document.getElementById('history-div').appendChild(div);
-// }
+};
